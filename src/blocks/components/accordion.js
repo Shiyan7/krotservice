d5a261c3856ff7ca93b244fbffb471867478e1f8
Shiyan7 <57950265+Shiyan7@users.noreply.github.com>
2021-11-12 14:43:17 +0200

    fix accordion

--- src/blocks/components/accordion.js
+++ src/blocks/components/accordion.js
@@ -1,4 +1,41 @@
 document.addEventListener("DOMContentLoaded", () => {
+    document.addEventListener("click", (e) => {
+        if(e.target.closest(".accordion")) {
+            let self = e.target.closest(".accordion");
+            const control = self.querySelector(".accordion__control");
+            const content = self.querySelector(".accordion__content");
+
+            if(e.target.closest(".accordion__control")) {
+                self.classList.toggle("open");
+            }
+
+            const watchHeight = () => {
+                content.style.maxHeight = content.scrollHeight + "px";
+            };
+
+            window.addEventListener("resize", () => {
+                watchHeight();
+            });
+
+            // если открыт аккордеон
+            if (self.classList.contains("open")) {
+                control.setAttribute("aria-expanded", true);
+                content.setAttribute("aria-hidden", false);
+                watchHeight();
+            } else {
+                control.setAttribute("aria-expanded", false);
+                content.setAttribute("aria-hidden", true);
+                content.style.maxHeight = null;
+            }
+        }
+    });
+});
+
+
+
+
+
+/* document.addEventListener("DOMContentLoaded", () => {
     const accordions = document.querySelectorAll(".accordion");
 
     accordions.forEach(el => {
@@ -29,4 +66,4 @@ document.addEventListener("DOMContentLoaded", () => {
             }
         });
     });
-});
+}); */
